12feae68322ea9ac821d227b383fe680388a1f8f
zdtz <dduythien.it@gmail.com>
2023-08-18 00:04:47 +0700

    check axios

--- src/api/index.js
+++ src/api/index.js
@@ -1,30 +1,38 @@
-import request from "./request";
+// import request from "./request";
 // import { IPaymentSession, IPaymentSessionResponse } from "@services/models";
-export const authenticate = (payload) => {
-  return request("http://103.2.230.53:8089/api/auth/authenticate", {
-    method: "POST",
-    data: payload,
+export const authenticate = async (payload) => {
+  const data = await fetch("http://103.2.230.53:8089/api/auth/authenticate", {method: "POST", body: payload})
+  .then((res) => res.json())
+  .then((res) => {
+     return res
+   })
+  .catch((error) => {
+     return error
   });
+  return data
 };
 
 export const createParkingSessionService = (payload) => {
-  return request("http://103.2.230.53:8089/api/parkingsession/get", {
-    method: "POST",
-    data: payload,
-  });
+  // return request("http://103.2.230.53:8089/api/parkingsession/get", {
+  //   method: "POST",
+  //   data: payload,
+  // });
+  return {}
 };
 
 export const markParkingSessionService = (payload) => {
-  return request("http://103.2.230.53:8089/api/parkingsession/mark-payment", {
-    method: "POST",
-    data: payload,
-  });
+  // return request("http://103.2.230.53:8089/api/parkingsession/mark-payment", {
+  //   method: "POST",
+  //   data: payload,
+  // });
+  return {}
 };
 
 
 export const paymentService = (payload) => {
-  return request("https://test-payment.momo.vn/v2/gateway/api/create", {
-    method: "POST",
-    data: payload,
-  });
+  // return request("https://test-payment.momo.vn/v2/gateway/api/create", {
+  //   method: "POST",
+  //   data: payload,
+  // });
+  return {}
 };
